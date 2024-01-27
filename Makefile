8cda95574ff90240b9ebadc041508f21b6770178
Robert Lowe <robertmlowe@rmlowe.com>
2024-01-27 22:43:24 +0000

    162. Overloading Operators as Member Functions

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ Mystring-move-assignment - Debug ]----------"
-	@cd "Mystring-move-assignment" && "$(MAKE)" -f  "Mystring-move-assignment.mk"
+	@echo "----------Building project:[ Mystring-operator-methods - Debug ]----------"
+	@cd "Mystring-operator-methods" && "$(MAKE)" -f  "Mystring-operator-methods.mk"
 clean:
-	@echo "----------Cleaning project:[ Mystring-move-assignment - Debug ]----------"
-	@cd "Mystring-move-assignment" && "$(MAKE)" -f  "Mystring-move-assignment.mk" clean
+	@echo "----------Cleaning project:[ Mystring-operator-methods - Debug ]----------"
+	@cd "Mystring-operator-methods" && "$(MAKE)" -f  "Mystring-operator-methods.mk" clean
---
+++ Mystring-operator-methods/Mystring.h
@@ -0,0 +1,28 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+private:
+    char *str;  // pointer to a char[] that holds a C-style string
+public:
+    Mystring();                                 // No-args contstructor
+    Mystring(const char *s);                    // Overloaded contstructor
+    Mystring(const Mystring &source);           // Copy constructor
+    Mystring( Mystring &&source);               // Move constructor
+    ~Mystring();                                // Destructor
+    
+    Mystring &operator=(const Mystring &rhs);   // Copy assignment
+    Mystring &operator=(Mystring &&rhs);        // Move assignment
+    
+    Mystring operator-() const;                 // make lowercase
+    Mystring operator+(const Mystring &rhs) const;    // concatenate
+    bool operator==(const Mystring &rhs) const; 
+    
+    void display() const;
+    
+    int get_length() const;                     // getters
+    const char *get_str() const;
+};
+
+#endif // _MYSTRING_H_
